78c5f29d100d4ba4643100a030cc704dda4e93cc
bayuik <bayuindrakusuma05@gmail.com>
2025-06-01 16:36:49 +0700

    fix: change domain to bayuik.com

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -60,7 +60,7 @@ export const guest = (() => {
     const showGuestName = () => {
         /**
          * Make sure "to=" is the last query string.
-         * Ex. ulems.my.id/?id=some-uuid-here&to=name
+         * Ex. natasya.bayuik.com/?id=some-uuid-here&to=name
          */
         const raw = window.location.search.split('to=');
         let name = null;
